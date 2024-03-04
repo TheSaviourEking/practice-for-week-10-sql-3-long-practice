a1640889f7b32f1baf2c06814d3d636d4bcc2e7d
TheSaviourEking <saviour.eking@outlook.com>
2024-03-04 18:38:34 +0100

    chore: implement phase-2.sql

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,2 +1,27 @@
 -- Using subqueries, find the names of the cats whose owners are either George Beatty or Melynda Abshire
 -- Your code here
+SELECT DISTINCT
+  name
+FROM
+  cats
+WHERE
+  id IN (
+    SELECT
+      cat_id
+    FROM
+      cat_owners
+    WHERE
+      owner_id IN (
+        SELECT
+          id
+        FROM
+          owners
+        WHERE
+          (
+            first_name = 'George'
+            AND last_name = 'Beatty'
+          )
+          OR first_name = 'Melynda'
+          AND last_name = 'Abshire'
+      )
+  );
