85ee9fdcc2ed081740e55d94abd1aa21c465a115
TheSaviourEking <saviour.eking@outlook.com>
2024-03-05 23:16:00 +0100

    chore: implement Advanced Phase 4 INSERT Multiple Rows Order By Aggregate

--- sql-practice/phase-4.sql
+++ sql-practice/phase-4.sql
@@ -1,5 +1,41 @@
 -- Give "Red" the cat one of every toy the other cats have
 -- Your code here
-
 -- Query spoiled cats reporting the most spoiled first
 -- Your code here
+-- INSERT INTO
+--   toys (cat_id, name)
+-- SELECT
+--   (
+--     SELECT
+--       id
+--     FROM
+--       cats
+--     WHERE
+--       name = 'Red'
+--   ),
+--   name
+-- FROM
+--   toys
+-- WHERE
+--   cat_id <> (
+--     SELECT
+--       id
+--     FROM
+--       cats
+--     WHERE
+--       name = 'Red'
+--   )
+-- GROUP BY
+--   toys.name;
+SELECT
+  'Cat:',
+  cats.name,
+  'owns',
+  COUNT(toys.name) AS toys_count
+FROM
+  toys
+  JOIN cats ON (cats.id = toys.cat_id)
+GROUP BY
+  cats.id
+ORDER BY
+  toys_count DESC;
